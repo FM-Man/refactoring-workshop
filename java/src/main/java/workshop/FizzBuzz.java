8feae63a43af198f26d574617447f9fddc9c7512
fahim-morshed-1102 <bsse1102@iit.du.ac.bd>
2022-02-16 06:43:44 +0600

    Removed strReturn

--- java/src/main/java/workshop/FizzBuzz.java
+++ java/src/main/java/workshop/FizzBuzz.java
@@ -8,13 +8,9 @@ package workshop;
  */
 public class FizzBuzz {
     public static String say(int number) {
-        String strReturn;
-
-        if (number % 15 == 0) strReturn = "FizzBuzz";
-        else if (number % 5 == 0) strReturn = "Buzz";
-        else if (number % 3 == 0) strReturn = "Fizz";
-        else strReturn = String.valueOf(number);
-
-        return strReturn;
+        if (number % 15 == 0) return "FizzBuzz";
+        else if (number % 5 == 0) return "Buzz";
+        else if (number % 3 == 0) return "Fizz";
+        else return String.valueOf(number);
     }
 }
