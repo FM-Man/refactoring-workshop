845ccfb5b6e419a4aaaedb5974511099a55734db
fahim-morshed-1102 <bsse1102@iit.du.ac.bd>
2022-02-16 06:40:47 +0600

    Removed null object from FizzBuzz

--- java/src/main/java/workshop/FizzBuzz.java
+++ java/src/main/java/workshop/FizzBuzz.java
@@ -8,18 +8,13 @@ package workshop;
  */
 public class FizzBuzz {
     public static String say(int number) {
-        String strReturn = null;
+        String strReturn;
 
-        if (number % 15 == 0) {
-            strReturn = "FizzBuzz";
-        } else {
-            if (number % 3 == 0) strReturn = "Fizz";
+        if (number % 15 == 0) strReturn = "FizzBuzz";
+        else if (number % 5 == 0) strReturn = "Buzz";
+        else if (number % 3 == 0) strReturn = "Fizz";
+        else strReturn = String.valueOf(number);
 
-            if (number % 5 == 0) strReturn = "Buzz";
-        }
-
-        if (strReturn != null) return strReturn;
-
-        return String.valueOf(number);
+        return strReturn;
     }
 }
